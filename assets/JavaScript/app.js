0b13731a673d08092fa102714b4e60fd43d2c1b4
Kelvin Almonte <kelvin.almonte95@gmail.com>
2019-12-16 17:39:43 -0500

    Major changes for the portfolio page.

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,7 +1,36 @@
 // Portfolio Page
 
-$(".more-button").on("click", function(event) {
-    event.preventDefault();
-    // $(".more-button").hide();
+$(".fullStack-projects").hide();
+$(".backend-projects").hide();
+// $(".nodeJS-projects").hide();
 
+// This function will hide the following projects.
+function hideProjects() {
+    $(".frontend-projects").hide();
+    $(".backend-projects").hide();
+    $(".fullStack-projects").hide();
+}
+
+// Frontend projects
+$("#frontend").on("click", function(event) {
+    event.preventDefault();
+
+    hideProjects();
+    $(".frontend-projects").show();
 });
+
+// Backend projects
+$("#backend").on("click", function() {
+    event.preventDefault();
+
+    hideProjects();
+    $(".backend-projects").show();
+});
+
+// Full Stack projects
+$("#full-stack").on("click", function(event) {
+    event.preventDefault();
+
+    hideProjects();
+    $(".fullStack-projects").show();
+})
